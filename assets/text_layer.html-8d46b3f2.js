92bf6b1c818f0df578e9e0afb31030ee7582931e
TaTo30 <aldo.hernandez3012@gmail.com>
2023-07-15 13:48:54 -0600

    docs improved

---
+++ assets/text_layer.html-8d46b3f2.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-1570d88c","path":"/examples/basic/text_layer.html","title":"Text-layer","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1688955190000,"contributors":[{"name":"TaTo30","email":"aldo.hernandez3012@gmail.com","commits":4}]},"filePathRelative":"examples/basic/text_layer.md"}');export{e as data};
